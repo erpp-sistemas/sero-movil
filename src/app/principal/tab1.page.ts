01a5e82735a33b119af33460b6de139a2de63156
Antonio Ezequiel Ticante Pèrez <erpp.sistemas@gmail.com>
2021-08-29 10:37:40 -0500

    Nueva logica para mas dinamismo en la peticion y envio de datos

--- src/app/principal/tab1.page.ts
+++ src/app/principal/tab1.page.ts
@@ -37,15 +37,15 @@ export class Tab1Page implements OnInit {
   descargaPredio: boolean;
   descargaAntenas: boolean;
   descargaPozos: boolean;
-  plazas: any = [];
-  idPlazas: any = [];
   id_plaza: number = 0; // select option de plaza
   plaza: number = 0;
   selecciona: boolean = false;
+  plazasServicios: any;
+  servicios: any;
 
   plazaServicioDescarga = [];
   login: boolean;
-
+  descargaServicio: boolean = false;
   constructor(
     private storage: Storage,
     private rest: RestService,
@@ -69,7 +69,7 @@ export class Tab1Page implements OnInit {
 
   ionViewDidEnter() {
     this.platform.ready().then(() => {
-      this.obtenerPlazasUsuario();
+      //this.obtenerPlazasUsuario();
     });
   }
 
@@ -85,10 +85,12 @@ export class Tab1Page implements OnInit {
    * Metodo que obtiene las plazas y los ids guardadas en el storage por auth.service
    */
   async obtenerPlazasUsuario() {
-    let plazasRest = this.rest.obtenerPlazas();
-    this.plazas = (await plazasRest).plazas;
-    this.idPlazas = (await plazasRest).idPlazas;
-    this.id_plaza = await this.idPlazas[0];
+
+    this.plazasServicios = await this.rest.obtenerPlazasSQL();
+    console.log(this.plazasServicios);
+
+    this.id_plaza = this.plazasServicios[0].id_plaza;
+
     const servicios = await this.rest.mostrarServicios(this.id_plaza);
     // En este punto ya se tiene el primer id_plaza obtenido de la base
     this.mostrarServicios(servicios);
@@ -99,9 +101,9 @@ export class Tab1Page implements OnInit {
    * @param event 
    */
   async resultPlaza(event) {
-
+    console.log(event.detail.value);
     console.log("idPlaza: " + this.id_plaza);
-
+    console.log("Este es el id_plaza en el result " + this.id_plaza);
     // si el idPlaza es diferente de 0 entonces verificar la descarga
     if (this.id_plaza != 0) {
       // verificar si el id_plaza escogida tiene un estatus true en el campo descargado de la tabla descargaServicios
@@ -113,6 +115,7 @@ export class Tab1Page implements OnInit {
       this.asignarSectores(this.id_plaza);
     }
   }
+
 
   /**
    * Metodo que trae la informacion de la plaza para ver si ya fue descargada la informacion
@@ -145,7 +148,7 @@ export class Tab1Page implements OnInit {
   }
 
   /**
-   * Metodo que activa los servicios segun la plaza que se pasa por parametro
+   * Metodo que activa los servicios segun la plaza que se pasa por parametro viene del result
    * @param idPlaza 
    */
   async asignarSectores(idPlaza) {
@@ -157,29 +160,17 @@ export class Tab1Page implements OnInit {
       this.selecciona = false;
     }
 
-    const servicios = await this.rest.mostrarServicios(idPlaza);
-    this.mostrarServicios(servicios);
+    this.servicios = await this.rest.mostrarServicios(idPlaza);
+    this.mostrarServicios(this.servicios);
 
   }
 
+  // viene del obtenerPlazasUsuario
+  async mostrarServicios(servicios) {
+    console.log(servicios);
+    this.servicios = await this.rest.mostrarServicios(this.id_plaza)
 
-  mostrarServicios(servicios) {
-    servicios.forEach(servicio => {
-      if (servicio.id_servicio == 1) {
-        this.agua = true;
-      } else if (servicio.id_servicio == 2) {
-        this.predio = true
-      } else if (servicio.id_servicio == 3) {
-        this.antenas = true;
-      } else if (servicio.id_servicio == 4) {
-        this.pozos = true;
-      }
-    });
   }
-
-  // async mostrarAvatar() {
-  //   this.imgAvatar += await this.storage.get('ImgAvatar');
-  // }
 
   /**
    * Metodo que obtiene el nombre y el email del usuario desde el storage
@@ -192,29 +183,11 @@ export class Tab1Page implements OnInit {
 
 
 
-  async confirmarDescarga(tipo) {
-
-    if (tipo == 1) {
-      console.log("Descargar agua en la plaza " + this.id_plaza);
-      this.confirmaDescargaAgua(this.id_plaza)
-    } else if (tipo == 2) {
-      console.log("Descarga predio en la plaza " + this.id_plaza);
-      this.confirmarDescargaPredio(this.id_plaza);
-    } else if (tipo == 3) {
-      console.log("Descarga antenas");
-      console.log("Descarga predio en la plaza " + this.id_plaza);
-      this.confirmarDescargaAntenas(this.id_plaza);
-      // llamar al metodo confirme la descarga de pozos
-    } else if (tipo == 4) {
-      console.log("Descarga pozos");
-      // llamar al metodo confirme la descarga de antenas
-    }
-  }
-
-  async confirmaDescargaAgua(id_plaza) {
+  async confirmarDescarga(tipo, plaza, servicio) {
+    console.log(tipo, plaza, servicio);
     const alert = await this.alertCtrl.create({
-      subHeader: 'Descarga de cuentas de agua',
-      message: 'Confirme para iniciar la descarga',
+      header: plaza.toUpperCase(),
+      subHeader: "Confirme para iniciar la descarga de " + servicio,
       buttons: [
         {
           text: "Cancelar",
@@ -229,98 +202,20 @@ export class Tab1Page implements OnInit {
           cssClass: "secondary",
           handler: () => {
             //  if(this.network.getNetworkType() == 'wifi'){
-            this.descargarInformacionAgua(id_plaza);
+            this.descargarInformacion(this.id_plaza, tipo);
           }
         }
       ]
     });
     await alert.present();
+
   }
 
 
-  async confirmarDescargaPozos() {
-    const alert = await this.alertCtrl.create({
-      subHeader: 'Descarga de datos de Pozos',
-      message: 'Confirme para iniciar descarga',
-      buttons: [
-        {
-          text: "Cancelar",
-          role: "cancel",
-          cssClass: "secondary",
-          handler: blah => {
-            console.log("Confirm Cancel: blah");
-          }
-        },
-        {
-          text: "Confirmar",
-          cssClass: "secondary",
-          handler: () => {
-            //  if(this.network.getNetworkType() == 'wifi'){
-            this.descargarInfoPozos();
-          }
-        }
-      ]
-    });
-    await alert.present();
-  }
-
-  async confirmarDescargaPredio(id_plaza) {
-    const alert = await this.alertCtrl.create({
-      subHeader: 'Descarga de cuentas de predio',
-      message: 'Confirme para iniciar descarga',
-      buttons: [
-        {
-          text: "Cancelar",
-          role: "cancel",
-          cssClass: "secondary",
-          handler: blah => {
-            console.log("Confirm Cancel: blah");
-          }
-        },
-        {
-          text: "Confirmar",
-          cssClass: "secondary",
-          handler: () => {
-            //  if(this.network.getNetworkType() == 'wifi'){
-            this.descargarInfoPredio(id_plaza);
-          }
-        }
-      ]
-    });
-    await alert.present();
-  }
-
-  async confirmarDescargaAntenas(id_plaza) {
-    const alert = await this.alertCtrl.create({
-      subHeader: 'Descarga de cuentas de antenas',
-      message: 'Confirme para iniciar descarga',
-      buttons: [
-        {
-          text: "Cancelar",
-          role: "cancel",
-          cssClass: "secondary",
-          handler: blah => {
-            console.log("Confirm Cancel: blah");
-          }
-        },
-        {
-          text: "Confirmar",
-          cssClass: "secondary",
-          handler: () => {
-            //  if(this.network.getNetworkType() == 'wifi'){
-            this.descargarInfoAntenas(id_plaza);
-          }
-        }
-      ]
-    });
-    await alert.present();
-  }
-
-
-  async descargarInformacionAgua(id_plaza) {
-
+  async descargarInformacion(id_plaza, tipo) {
     this.progress = true;
-    this.deleteInfoAgua();
+    // aqui solo abra un metodo para borrar la informacion deleteInfo
+    this.deleteInfo(tipo);
 
     this.loading = await this.loadinCtrl.create({
       message: 'Descargando informaciòn...',
@@ -331,7 +226,8 @@ export class Tab1Page implements OnInit {
     const idaspuser = await this.storage.get("IdAspUser");
 
     try {
-      this.data = await this.rest.obtenerDatosSqlAgua(idaspuser, id_plaza);
+
+      this.data = await this.rest.obtenerDatosSql(idaspuser, id_plaza, tipo);
       this.total = this.data.length;
 
       if (this.total == 0) {
@@ -342,7 +238,7 @@ export class Tab1Page implements OnInit {
 
       this.storage.set("total", this.total);
 
-      await this.guardarDatosSQLAgua(this.data, id_plaza);
+      await this.guardarDatosSQL(this.data, id_plaza, tipo);
 
       // Aqui se guardaran las llaves en el storage para definir campos de validacion de modulos
 
@@ -362,184 +258,58 @@ export class Tab1Page implements OnInit {
       let fecha = ionicDate.toISOString();
       this.storage.set("FechaSync", fecha);
       this.loading.dismiss();
-      await this.storage.set('DescargaAgua', true);
 
-      this.descargaAgua = true;
 
       // se descargo agua en la plaza this.id_plaza, meter en la tabla descargaServicios
-      this.actualizarEstatusDescarga(this.id_plaza, 1); // 1 es agua
+      
+      this.actualizarEstatusDescarga(this.id_plaza, tipo); // 1 es agua
 
 
       console.log(this.plazaServicioDescarga);
       this.message.showAlert("Se han descargado tus cuentas!!!!");
-      //this.router.navigateByUrl('/home/tab2');
     } catch (eror) {
       this.message.showAlert("Error al intentar la descarga");
     }
   }
 
 
-  async guardarDatosSQLAgua(data, id_plaza) {
-    console.log("Guardando la informacion de agua");
+  async guardarDatosSQL(data, id_plaza, tipo) {
+    console.log("Guardando la informacion de " + tipo);
     console.log(data);
     let cont = 0;
     for (let i = 0; i < data.length; i++) {
-      await this.rest.guardarInfoSQLAgua(data[i], id_plaza);
+      await this.rest.guardarInfoSQL(data[i], id_plaza, tipo);
       cont = cont + 1;
       this.progressTotal = cont / this.total;
     }
   }
 
   async actualizarEstatusDescarga(id_plaza, servicio) {
-    await this.rest.guardarEstatusDescarga(id_plaza, servicio);
-  }
-
-  async descargarInfoPozos() {
-    this.deleteInfoPozos();
-    const idPlaza = await this.storage.get('IdPlaza');
-    this.loading = await this.loadinCtrl.create({
-      message: 'Descargando informaciòn de pozos...',
-      spinner: 'bubbles'
-    });
-
-    await this.loading.present();
-
-    try {
-      this.dataPozos = await this.rest.obtenerDatosPozos(idPlaza);
-      this.totalPozos = this.dataPozos.length;
-      if (this.totalPozos == 0) {
-        this.message.showAlert("No hay resultados, descargar datos de pozos");
-        this.loading.dismiss();
-        return;
-      }
-
-      await this.guardarDatosPozos(this.dataPozos);
-
-      this.loading.dismiss();
-      this.message.showAlert("Se han descargado tu informaciòn de pozos!!!!");
-
-      this.router.navigateByUrl('/home/tab3');
-    } catch (error) {
-
-    }
-
-
-  }
-
-  async guardarDatosPozos(data) {
-    for (let i = 0; i < data.length; i++) {
-      await this.rest.guardarInfoSqlPozos(data[i]);
-    }
+    await this.rest.actualizaServicioEstatus(id_plaza, servicio);
+    this.asignarSectores(this.id_plaza);
   }
 
 
-  async descargarInfoPredio(id_plaza) {
-    this.progress = true;
-    this.deleteInfoPredio();
 
-    this.loading = await this.loadinCtrl.create({
-      message: 'Descargando informaciòn...',
-      spinner: 'dots'
-    });
-
-    await this.loading.present();
-
-    const idaspuser = await this.storage.get("IdAspUser");
-
-    try {
-      this.data = await this.rest.obtenerDatosSqlPredio(idaspuser, id_plaza);
-      this.total = this.data.length;
-
-      if (this.total == 0) {
-        this.message.showAlert("No tienes cuentas asignadas para sincronizar");
-        this.loading.dismiss();
-        return;
-      }
-
-      this.storage.set("total", this.total);
-
-      await this.guardarDatosSQLPredio(this.data, id_plaza);
-
-      // Aqui se guardaran las llaves en el storage para definir campos de validacion de modulos
-
-      var dateDay = new Date().toISOString();
-      let date: Date = new Date(dateDay);
-      let ionicDate = new Date(
-        Date.UTC(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate(),
-          date.getHours(),
-          date.getMinutes(),
-          date.getSeconds(),
-        )
-      );
-
-      let fecha = ionicDate.toISOString();
-      this.storage.set("FechaSync", fecha);
-      this.loading.dismiss();
-      await this.storage.set('DescargaPredio', true);
-
-      this.descargaPredio = true;
-      // se descargo agua en la plaza this.id_plaza, meter en la tabla descargaServicios
-      this.actualizarEstatusDescarga(this.id_plaza, 2); // 2 es predio
-      console.log(this.plazaServicioDescarga);
-      this.message.showAlert("Se han descargado tus cuentas!!!!");
-
-    } catch (eror) {
-      this.message.showAlert("Error al intentar la descarga");
-    }
+  async deleteInfo(tipo) {
+    await this.rest.deleteTable(tipo);
   }
 
-  async guardarDatosSQLPredio(data, id_plaza) {
-    console.log("Guardando la informacion de predio");
-    let cont = 0;
-    for (let i = 0; i < data.length; i++) {
-      await this.rest.guardarInfoSQLPredio(data[i], id_plaza);
-      cont = cont + 1;
-      this.progressTotal = cont / this.total;
-    }
-  }
-
-  descargarInfoAntenas(id_plaza) {
-    this.message.showAlert("No tienes cuentas asignadas para sincronizar");
-  }
-
-  async guardarDatosSQLAntenas(data) {
-
-  }
-
-
-  async deleteInfoAgua() {
-    await this.rest.deleteTableAgua();
-  }
-
-  async deleteInfoPozos() {
-    await this.rest.deleteTablePozos();
-  }
-
-  async deleteInfoPredio() {
-    await this.rest.deleteTablePredio();
-  }
-
-  async deleteInfoAntenas() {
-
-  }
 
 
   async goCuentasTab(tipo) {
     //this.router.navigateByUrl('/home/tab2');
-    if( tipo == '1') {
+    if (tipo == '1') {
       await this.storage.set('tipo', 'Agua');
-    } else if(tipo == '2') {
+    } else if (tipo == '2') {
       await this.storage.set('tipo', 'Predio');
     }
     console.log("Tipo " + tipo);
-    
+
 
     const plaza_servicio = await this.rest.mostrarServicios(this.id_plaza);
 
-    const plaza = plaza_servicio.filter( e => {
+    const plaza = plaza_servicio.filter(e => {
       return e.id_plaza == this.id_plaza
     })
 
@@ -550,10 +320,10 @@ export class Tab1Page implements OnInit {
     this.router.navigate(['/listado-cuentas', tipo, this.id_plaza]);
   }
 
-  goMapTab( tipo ) {
+  goMapTab(tipo) {
     if (tipo == '1') {
       this.router.navigate(['/google-maps', this.id_plaza]);
-    } else if( tipo == '2') {
+    } else if (tipo == '2') {
       this.router.navigate(['/mapa-predio', this.id_plaza]);
     }
   }
